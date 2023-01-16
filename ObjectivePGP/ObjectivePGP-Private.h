4675895752d99406278562c56444e17a1d8c2119
Piyush Chopra <piyush.chopra@nagarro.com>
2023-01-16 09:45:19 +0530

    Moving bignum to public. Fixing a script issue.

--- ObjectivePGP/ObjectivePGP-Private.h
+++ ObjectivePGP/ObjectivePGP-Private.h
@@ -38,7 +38,6 @@ FOUNDATION_EXPORT const unsigned char ObjectivePGPVersionString[];
 #import <ObjectivePGP/PGPPacket.h>
 #import <ObjectivePGP/NSData+PGPUtils.h>
 #import <ObjectivePGP/PGPUser+Private.h>
-#import <ObjectivePGP/PGPBigNum.h>
 #import <ObjectivePGP/PGPCurveOID.h>
 #import <ObjectivePGP/PGPKeyMaterial.h>
 #import <ObjectivePGP/PGPMPI.h>
